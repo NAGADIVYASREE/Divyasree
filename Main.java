dea560a5b28b299a47cca1570a60d6ca0b08ce89
NAGADIVYASREE <31818392+NAGADIVYASREE@users.noreply.github.com>
2017-09-11 13:51:35 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args)
+	 {
+ StudentGroup b = new StudentGroup();
+ Student s[2]=new Student(int id, String fullName, Date birthDate, double avgMark);
+ b s.setStudents(Student[] students);
+
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+	}
+
+}
